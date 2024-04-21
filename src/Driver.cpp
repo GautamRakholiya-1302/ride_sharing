628013ed4e04cdec09f800a2f2f0718be8b637a8
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-04-21 21:31:50 +0530

    Update Driver.cpp

--- src/Driver.cpp
+++ src/Driver.cpp
@@ -1,1 +1,17 @@
+class Driver {
+public:
+    int driverID;
+    std::string name;
+    bool isAvailable;
+    Location currentLocation;
+
+    Driver(int id, std::string name, Location location) : driverID(id), name(name), currentLocation(location), isAvailable(true) {}
+
+    void updateLocation(const Location& newLocation) {
+        currentLocation = newLocation;
+    }
+
+    void acceptRide(int rideID);
+    void completeRide(int rideID);
+};
 
