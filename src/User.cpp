c5fd93aea801ec637d8203740b6fe1f45ded54e1
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-04-21 21:32:08 +0530

    Update User.cpp

--- src/User.cpp
+++ src/User.cpp
@@ -1,1 +1,14 @@
+#include <string>
+
+class User {
+public:
+    int userID;
+    std::string name;
+    std::string email;
+
+    User(int id, std::string name, std::string email) : userID(id), name(name), email(email) {}
+
+    void requestRide(const Location& startLocation, const Location& endLocation);
+    void rateDriver(int rating, int rideID);
+};
 
