9177846f121bdd72504d51a87e5a5b7b37506502
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-04-21 21:32:26 +0530

    Update Location.cpp

--- src/Location.cpp
+++ src/Location.cpp
@@ -1,1 +1,13 @@
+class Location {
+public:
+    double latitude;
+    double longitude;
+
+    Location(double lat, double lon) : latitude(lat), longitude(lon) {}
+
+    double calculateDistance(const Location& other) const {
+        // Simplistic distance calculation (not accurate for real-world scenarios)
+        return sqrt(pow(latitude - other.latitude, 2) + pow(longitude - other.longitude, 2));
+    }
+};
 
